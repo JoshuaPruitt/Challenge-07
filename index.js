31a9136080098b44b632791ef3078e4c8c1b8b69
JoshuaPruitt <JoshuaPruitt6484@gmail.com>
2024-07-25 14:09:05 -0600

    added more base files

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 // TODO: Include packages needed for this application
+import fs from 'fs'
+import inquire from "inquire"
+import colors from "colors"
 
 // TODO: Create an array of questions for user input
 const questions = [];
+
+inquire
+    .prompt([
+        {
+
+        }
+    ])
+    .then((prompt) =>
+
+    );
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
