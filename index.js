8439e82ed7d2fe413ce592a09260a6e589d0ea69
JoshuaPruitt <JoshuaPruitt6484@gmail.com>
2024-07-28 20:11:05 -0600

    fixed inquirer bug, testing code

--- index.js
+++ index.js
@@ -1,132 +1,148 @@
 // TODO: Include packages needed for this application
-import fs from 'fs'
-import inquire from "inquire"
+import * as fs from 'fs'
+import inquirer from "inquirer"
 import colors from "colors"
 import { constants } from 'buffer';
+
+let codeSnippitInsert = "````"
 
 //controls the 
 let githubConditional =  {
     type: 'input',
     message: "What is your github link?",
     name: 'contactMeGitHub',
-    
-
 }
 
 //index number
 let i;
 
-
-//use object Contructors to complete assignment. Provides blueprint for an object
-function readMe (intro, snippit){
-    this.intro = `This is the intro for the readme`;
-    this.snippit = `This is the code snippit`;
-}
-
-// TODO: Create an array of questions for user input
-const questions = [];
-
-inquire
+inquirer
     .prompt([
         {
-            type: 'prompt',
+            type: 'input',
             message: "What is your programs title?",
             name: 'title',
-            when(){
-                i++
-            }
         },
 
         {
-            type: 'prompt',
+            type: 'input',
             message: "What does your program require to be installed?",
             name: 'installation',
-            when(){
-                i++
-            }
         },
 
         {
-            type: 'prompt',
+            type: 'input',
             message: "How do you use your program?",
             name: 'usage',
-            when(){
-                i++
-            }
+        
+        },
+
+        {
+            type: 'input',
+            message: "What is an important piece of code from your program? (code please)",
+            name: 'codeSnippit',
             
         },
 
         {
-            type: 'prompt',
-            message: "What is an important piece of code from your program? (code please)",
-            name: 'codeSnippit',
-            when(){
-                i++
-            }
-        },
-
-        {
-            type: 'prompt',
+            type: 'input',
             message: "How does that piece of code work?",
             name: 'codeSnippitIntro',
-            when(){
-                i++
-            }
         },
 
         {
-            type: 'prompt',
+            type: 'input',
             message: "Who are the credits for your code?",
             name: 'credits',
-            when(){
-                i++
-            }
         },
 
         {
             type: 'confirm',
             message: "Do you wish to add a GitHub link?",
             name: 'contactMe',
-            when() {
-                i++
-                
-                if (this.contactMe){
-                    //splice the follow Up question into the array
-                    prompt.splice(i+1, 0, githubConditional)
-                }
+            when(answers) {
+
+                //splice the follow Up question into the array
+                prompt.splice(i+1, 0, githubConditional)
             }
         },
 
-       
-
         {
-            type: 'prompt',
+            type: 'input',
             message: "What programs did you use to create your program?",
             name: 'technologies',
-            when(){
-                i++
-            }
         },
     ])
 
     .then((prompt) => {
-        let template = fs.readFile('./assets/template.md', 'utf8' ,(err) =>
-            err ? console.error(err) : console.log('Success!'));
+        let data = 
+`
+# ${prompt.title}
+
+## Introduction
+${prompt.intro}
+
+## Table of Contents
+## Table of Contents 
 
 
-        let retranslate = JSON.stringify(template)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Code Snippit](#code-snippit)
+* [Credits](#credits)
+* [License](#license)
+* [Contact Me](#contact-me)
+* [Technologies](#technologies-used)
 
+## Installation
+${prompt.instalation}.
+
+## Usage
+${prompt.usage}.
+
+## Code Snippit
+
+${prompt.codeSnippitIntro}.
+
+${codeSnippitInsert}
+${prompt.codeSnippit}.
+${codeSnippitInsert}
+
+## Credits
+
+${prompt.credits}.
+
+## Contact Me
+
+${prompt.contactMe}.
+            
+
+## Technologies Used
+
+${prompt.technologies}.
+`
+
+        //reads the template file
+        // let template = fs.readFile('./assets/template.txt', 'utf8' ,(err, data) => {
+
+        //     if (data !== undefined){
+        //         console.log(' File was read Successfully!')
+        //     } else {
+        //         console.error(err)
+        //     }
+        // });
+            
         //writes the prompt data into README.md
-        fs.writeFile('README.md', retranslate ,(err) =>
-            err ? console.error(err) : console.log('Success!'));
+        fs.writeFile('README.md', data ,(err) =>
+            err ? console.error(err) : console.log('File was written to Successfully!'));
         
     });
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+// // TODO: Create a function to initialize app
+// function init() {}
 
-// Function call to initialize app
-init();
+// // Function call to initialize app
+// init();
